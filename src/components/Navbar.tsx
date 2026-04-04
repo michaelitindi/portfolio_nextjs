1300d6eaebce66e5eb8997ec47ae6c4780ffb465
michaelitindi <michaelitindi@gmail.com>
2026-04-04 16:45:53 +0300

    style: change navbar CTA to 'Work with me'

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ export default function Navbar() {
             <Link href="/services">Services</Link>
             <Link href="/portfolio">Portfolio</Link>
             <Link href="/tools">Tools</Link>
-            <a href={process.env.NEXT_PUBLIC_TALLY_URL} target="_blank" rel="noopener noreferrer" className="btn-primary">See if you're a good fit</a>
+            <a href={process.env.NEXT_PUBLIC_TALLY_URL} target="_blank" rel="noopener noreferrer" className="btn-primary">Work with me</a>
           </div>
         </div>
       </div>
